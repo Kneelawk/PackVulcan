c93ae3a3999deea9dcadd1fd01a5401b873dc8a7
Kneelawk <jedidiah.pommert@gmail.com>
2022-02-28 15:58:14 -0800

    Fix main class reference

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ tasks.withType<KotlinCompile> {
 
 compose.desktop {
     application {
-        mainClass = "MainKt"
+        mainClass = "com.kneelawk.mrmpb.MainKt"
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "ModrinthModpackBuilder"
