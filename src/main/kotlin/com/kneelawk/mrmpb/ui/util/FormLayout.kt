bb09bd19ff05d4c4337924230a8cca51b2c0dead
Kneelawk <jedidiah.pommert@gmail.com>
2022-03-01 22:25:19 -0800

    Begin on FormLayout

--- src/main/kotlin/com/kneelawk/mrmpb/ui/util/FormLayout.kt
+++ src/main/kotlin/com/kneelawk/mrmpb/ui/util/FormLayout.kt
@@ -1,3 +1,38 @@
 package com.kneelawk.mrmpb.ui.util
 
+import androidx.compose.foundation.layout.LayoutScopeMarker
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
 
+/**
+ * A layout composable for creating form layouts. This is similar to a grid, but with some modifications.
+ */
+@Composable
+inline fun Form(
+    modifier: Modifier = Modifier,
+    columnWeights: FloatArray,
+    content: @Composable FormScope.() -> Unit
+) {
+//    Layout(
+//        content = { FormScopeInstance.content() },
+//        modifier = modifier,
+//        measurePolicy = { measurables, constraints ->
+//            layout()
+//        }
+//    )
+}
+
+/**
+ * Scope for the children of [Form].
+ */
+@LayoutScopeMarker
+@Immutable
+interface FormScope {
+
+}
+
+internal object FormScopeInstance : FormScope {
+
+}
