7fd5111b6e5a81833e114131373971609ea55ffb
Kneelawk <kneelawk@gmail.com>
2022-08-22 05:51:28 -0700

    Fix CategoryDisplay crashing on invalid SVGs

--- src/main/kotlin/com/kneelawk/packvulcan/ui/modrinth/CategoryDisplay.kt
+++ src/main/kotlin/com/kneelawk/packvulcan/ui/modrinth/CategoryDisplay.kt
@@ -1,5 +1,6 @@
 package com.kneelawk.packvulcan.ui.modrinth
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
@@ -16,12 +17,18 @@ import com.kneelawk.packvulcan.ui.util.loadSvgPainter
 import com.kneelawk.packvulcan.util.suspendGet
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import mu.KotlinLogging
 import java.util.*
 
-class CategoryDisplay(override val prettyName: String, val apiName: String, val projectType: String, override val icon: @Composable () -> Unit) :
+class CategoryDisplay(
+    override val prettyName: String, val apiName: String, val projectType: String,
+    override val icon: @Composable () -> Unit
+) :
     DisplayElement {
     companion object {
-        private fun getPainterFor(json: CategoryJson): Painter {
+        private val log = KotlinLogging.logger { }
+
+        private fun getPainterFor(json: CategoryJson): Painter? {
             return when (json.name) {
                 "adventure" -> PackVulcanIcons.compass
                 "cursed" -> PackVulcanIcons.bug
@@ -34,7 +41,14 @@ class CategoryDisplay(override val prettyName: String, val apiName: String, val 
                 "storage" -> PackVulcanIcons.inventory
                 "technology" -> PackVulcanIcons.microscope
                 "utility" -> PackVulcanIcons.fancySuitcase
-                else -> loadSvgPainter(json.icon, Density(1f))
+                else -> if (json.icon.isNotBlank()) {
+                    try {
+                        loadSvgPainter(json.icon, Density(1f))
+                    } catch (e: RuntimeException) {
+                        log.warn("Encountered invalid SVG for category: '{}'", json.name)
+                        null
+                    }
+                } else null
             }
         }
 
@@ -43,7 +57,13 @@ class CategoryDisplay(override val prettyName: String, val apiName: String, val 
                 if (it.isLowerCase()) it.titlecase(Locale.ENGLISH) else it.toString()
             }
             val icon = getPainterFor(json)
-            CategoryDisplay(prettyName, json.name, json.projectType) { Icon(icon, json.name, modifier = Modifier.size(24.dp)) }
+            CategoryDisplay(prettyName, json.name, json.projectType) {
+                if (icon != null) {
+                    Icon(icon, json.name, modifier = Modifier.size(24.dp))
+                } else {
+                    Box(modifier = Modifier.size(24.dp))
+                }
+            }
         }
 
         private val categoryCache: AsyncCache<Unit, List<CategoryDisplay>> =
